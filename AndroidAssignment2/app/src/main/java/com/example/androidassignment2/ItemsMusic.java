949bbd1bc8c01929fd5588021126a3f3f38c3df9
westers209 <jae509@gmail.com>
2019-04-06 16:02:19 -0400

    Add files via upload

---
+++ AndroidAssignment2/app/src/main/java/com/example/androidassignment2/ItemsMusic.java
@@ -0,0 +1,8 @@
+package com.example.androidassignment2;
+
+public class ItemsMusic {
+    public String title;
+    public String cover;
+    public String artist;
+    public String cost;
+}
---
+++ AndroidAssignment2/app/src/main/java/com/example/androidassignment2/MusicPop.java
@@ -0,0 +1,4 @@
+package com.example.androidassignment2;
+
+public interface MusicPop {
+}
